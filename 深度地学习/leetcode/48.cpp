48088dadbcba29cc5bed8c7d0c33e8a54d34eb56
Ren Guo <35659023+LittleChild@users.noreply.github.com>
2020-12-20 13:46:53 +0800

    Add files via upload

--- 深度地学习/leetcode/48.cpp
+++ 深度地学习/leetcode/48.cpp
@@ -9,27 +9,14 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-         int *state=new int[n*n];
-         for(int i=0;i<n;i++){
-             for(int j=0;j<n;j++){
-                 if(state[i*n+j]!=2){
-                    int t=matrix[i][j];
-                    matrix[i][j]=matrix[n-1-i][j];
-                    matrix[n-1-i][j]=t;
-                    state[i*n+j]=2;
-                    state[(n-1-i)*n+j]=2;
-                 }
-             }
-         }
-         for(int i=0;i<n;i++){
-             for(int j=0;j<n;j++){
-                 if(state[i*n+j]!=1){
-                    int t=matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=t;
-                    state[i*n+j]=1;
-                    state[j*n+i]=1;
-                 }
+        for(int i=0;i<n/2;i++){
+            matrix[i].swap(matrix[n-1-i]);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int t=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=t;
              }
          }
     }
