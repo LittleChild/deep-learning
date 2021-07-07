5d0b2d36b97aeef74a0932d5c884fe97cd266591
Ren Guo <35659023+LittleChild@users.noreply.github.com>
2021-07-07 23:35:34 +0800

    Create 819.cpp

---
+++ 深度地学习/leetcode/819.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        auto query = unordered_map<int, vector<vector<int>*>> {};
+        for (auto&& route : routes)
+            for (auto e : route)
+                query[e].push_back(&route);
+        auto q = queue { deque { source } };
+        for (auto cnt = 0; !q.empty(); ++cnt) {
+            for (auto i = q.size(); i; --i) {
+                auto f = q.front();
+                q.pop();
+                if (f == target) return cnt;
+                for (auto&& r : query[f]) {
+                    for (auto&& e : *r)
+                        q.emplace(e);
+                    r -> clear();
+                }
+            }
+        }
+        return -1;
+    }
+};
